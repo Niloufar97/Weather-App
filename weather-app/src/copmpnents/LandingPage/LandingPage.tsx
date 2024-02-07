99964d4e1fde801788563dd8eef0f6ed0458293e
Niloufar <nilooooofar97@gmail.com>
2024-02-07 21:02:42 +0100

    fetch to opneWeatherAPI

--- weather-app/src/copmpnents/LandingPage/LandingPage.tsx
+++ weather-app/src/copmpnents/LandingPage/LandingPage.tsx
@@ -1,4 +1,20 @@
+import { ChangeEvent, useState } from "react";
+const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+
 function LandingPage() {
+  const [term, setTerm] = useState<string>("");
+  const onChangeInput = (e : ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setTerm(value);
+    if(value === "") return;
+    getSearchOptions(value);
+  }
+  const getSearchOptions = (value:string) => {
+    
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+      apiKey
+    }`)
+  }
   return (
     <section className="w-full max-w-xl min-h-80 flex flex-col justify-center items-center bg-white bg-opacity-20 rounded backdrop-blur-lg text-zinc-700">
       <h1 className="text-3xl m-2">
@@ -8,8 +24,9 @@ function LandingPage() {
       <div className="flex mt-10 md:mt-5 ">
         <input
           type="text"
-          value={""}
+          value={term}
           className="border-white border-2 rounded-l-md px-2 py-1 "
+          onChange={onChangeInput}
         ></input>
         <button
           type="button"
