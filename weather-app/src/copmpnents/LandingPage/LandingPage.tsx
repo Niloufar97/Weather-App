48b4b1482e72ec0b77ce4f919afba10913d3f204
Niloufar <nilooooofar97@gmail.com>
2024-02-13 10:02:22 +0100

    get the result of forcast from api

--- weather-app/src/copmpnents/LandingPage/LandingPage.tsx
+++ weather-app/src/copmpnents/LandingPage/LandingPage.tsx
@@ -1,15 +1,19 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { optionsType } from "../../types";
 const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
 
 function LandingPage(): JSX.Element {
   const [term, setTerm] = useState<string>("");
   const [options, setOptions] = useState<[]>([]);
+  const [city, setCity] = useState<optionsType | null>(null);
+  // change fuction for input
   const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim();
     setTerm(value);
     if (value === "") return;
     getSearchOptions(value);
   };
+  // get options from search box
   const getSearchOptions = (value: string) => {
     fetch(
       `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${apiKey}`
@@ -17,6 +21,27 @@ function LandingPage(): JSX.Element {
       .then((response) => response.json())
       .then((data) => setOptions(data));
   };
+  // when user select one option from the list
+  const onOptionSelect = (option: optionsType) => {
+    setCity(option);
+    
+  };
+  useEffect(()=>{
+    if(city){
+      setTerm(city.name);
+      setOptions([]);
+    }
+  },[city]);
+  // when user click search button to submit the city
+  const onSubmit = () => {
+   if(!city) return
+   getForcast(city)
+  }
+  const getForcast = (city: optionsType) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => console.log({data}))
+  }
   return (
     <section className="w-full max-w-xl min-h-96 flex flex-col justify-center items-center bg-white bg-opacity-20 rounded backdrop-blur-lg text-zinc-700 py-2">
       <h1 className="text-3xl m-2">
@@ -31,10 +56,12 @@ function LandingPage(): JSX.Element {
           onChange={onChangeInput}
         ></input>
         <ul className="absolute bg-white top-9 rounded">
-          {options.map((option: { name: string }, index: number) => {
+          {options.map((option: optionsType, index: number) => {
             return (
               <li className="hover:bg-zinc-400 cursor-pointer" key={index}>
-                <button className="px-1 ">{option.name}</button>
+                <button className="px-1" onClick={() => onOptionSelect(option)}>
+                  {option.name}
+                </button>
               </li>
             );
           })}
@@ -42,6 +69,7 @@ function LandingPage(): JSX.Element {
         <button
           type="button"
           className="rounded-r-md border-2 border-zinc-100 px-2 py-1 cursor-pointer text-zinc-400 hover:border-zinc-500 hover:text-zinc-500 "
+          onClick={onSubmit}
         >
           Search
         </button>
