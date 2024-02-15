4f198489878df566d2e3ec1fe241e1153e02a5de
Niloufar <nilooooofar97@gmail.com>
2024-02-15 09:12:00 +0100

    define a search component to seprate serching function

---
+++ weather-app/src/copmpnents/Search.tsx
@@ -0,0 +1,50 @@
+import { ChangeEvent } from "react";
+import { optionsType } from "../types";
+type props = {
+    term : string,
+    options: [],
+    onChangeInput : (e : ChangeEvent<HTMLInputElement>) => void,
+    onOptionSelect : (option: optionsType) => void,
+    onSubmit : () => void
+}
+function Search({term , options , onChangeInput, onOptionSelect , onSubmit} : props) {
+  return (
+    <section className="w-full max-w-xl min-h-96 flex flex-col justify-center items-center bg-white bg-opacity-20 rounded backdrop-blur-lg text-zinc-700 py-2">
+        <h1 className="text-3xl m-2">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+        <p className="m-2">Enter your location:</p>
+        <div className="flex mt-10 md:mt-5 relative mb-8">
+          <input
+            type="text"
+            value={term}
+            className=" border-white border-2 rounded-l-md px-2 py-1 "
+            onChange={onChangeInput}
+          ></input>
+          <ul className="absolute bg-white top-9 rounded">
+            {options.map((option: optionsType, index: number) => {
+              return (
+                <li className="hover:bg-zinc-400 cursor-pointer" key={index}>
+                  <button
+                    className="px-1"
+                    onClick={() => onOptionSelect(option)}
+                  >
+                    {option.name}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+          <button
+            type="button"
+            className="rounded-r-md border-2 border-zinc-100 px-2 py-1 cursor-pointer text-zinc-400 hover:border-zinc-500 hover:text-zinc-500 "
+            onClick={onSubmit}
+          >
+            Search
+          </button>
+        </div>
+      </section>
+  )
+}
+
+export default Search;
--- weather-app/src/pages/LandingPage/LandingPage.tsx
+++ weather-app/src/pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import { optionsType } from "../../types";
+import Search from "../../copmpnents/Search";
 const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
 
 function LandingPage(): JSX.Element {
@@ -24,57 +25,29 @@ function LandingPage(): JSX.Element {
   // when user select one option from the list
   const onOptionSelect = (option: optionsType) => {
     setCity(option);
-    
   };
-  useEffect(()=>{
-    if(city){
+  useEffect(() => {
+    if (city) {
       setTerm(city.name);
       setOptions([]);
     }
-  },[city]);
+  }, [city]);
   // when user click search button to submit the city
   const onSubmit = () => {
-   if(!city) return
-   getForcast(city)
-  }
+    if (!city) return;
+    getForcast(city);
+  };
   const getForcast = (city: optionsType) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(data => console.log({data}))
-  }
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log({ data }));
+  };
   return (
-    <section className="w-full max-w-xl min-h-96 flex flex-col justify-center items-center bg-white bg-opacity-20 rounded backdrop-blur-lg text-zinc-700 py-2">
-      <h1 className="text-3xl m-2">
-        Weather <span className="font-black">Forecast</span>
-      </h1>
-      <p className="m-2">Enter your location:</p>
-      <div className="flex mt-10 md:mt-5 relative mb-8">
-        <input
-          type="text"
-          value={term}
-          className=" border-white border-2 rounded-l-md px-2 py-1 "
-          onChange={onChangeInput}
-        ></input>
-        <ul className="absolute bg-white top-9 rounded">
-          {options.map((option: optionsType, index: number) => {
-            return (
-              <li className="hover:bg-zinc-400 cursor-pointer" key={index}>
-                <button className="px-1" onClick={() => onOptionSelect(option)}>
-                  {option.name}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-        <button
-          type="button"
-          className="rounded-r-md border-2 border-zinc-100 px-2 py-1 cursor-pointer text-zinc-400 hover:border-zinc-500 hover:text-zinc-500 "
-          onClick={onSubmit}
-        >
-          Search
-        </button>
-      </div>
-    </section>
+    <main className="w-full max-w-xl flex flex-col justify-center items-center">
+      <Search term={term} options={options} onChangeInput={onChangeInput} onOptionSelect={onOptionSelect} onSubmit={onSubmit}/>
+    </main>
   );
 }
 
